2e763edeb05f39396a423fd37678f29f0e65479f
ThatCuteOne <gmascuto@gmail.com>
2026-01-19 21:30:49 +0100

    feat: auto meta.json update
    Adds a workflow for updating the meta.json

---
+++ .github/workflows/update-meta.py
@@ -0,0 +1,25 @@
+import json
+import time
+
+
+def main():
+    MODPACK_VERSION = ""
+    with open("pack.json","r") as f:
+        data:dict = json.load(f)
+        MODPACK_VERSION = data["pack_version"]
+    with open("meta.json","r") as pre_write:
+        data:dict = json.load(pre_write)
+        with open("meta.json","w") as write_file:
+            data["versions"].append(
+                {
+                    "id": MODPACK_VERSION,
+                    "releasedAt": int(time.time()),
+                    "promotions": {
+                        "downloads": {
+                            "generic": "https://github.com/ThatCuteOne/meowdpack/releases/latest"
+                        }
+                    }
+                }
+            )
+            json.dump(data,write_file,indent=2)
+
